fd583e0926ce25d2a57ff9c5cd57523af3f6aae6
seroroman <47804854+seroroman@users.noreply.github.com>
2021-11-02 14:36:33 +0900

    完了戻すの実装完了

--- src/index.js
+++ src/index.js
@@ -22,7 +22,31 @@ const onClickAdd = () => {
   completeButton.innerText = "完了";
 
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親タグdivを未完了リストから削除
+    deleteFromImcompleteList(completeButton.parentNode);
+
+    // 完了リストに追加する処理
+    const addTarget = completeButton.parentNode;
+
+    // TOTO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.innerText = text;
+    // 戻すボタン生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // div タグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)タグ作成
@@ -31,14 +55,20 @@ const onClickAdd = () => {
 
   deleteButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグdivを未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromImcompleteList(deleteButton.parentNode);
   });
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
+};
+
+// 未完了リストから指定の要素を削除
+
+const deleteFromImcompleteList = (target) => {
+  // 押された削除ボタンの親タグdivを未完了リストから削除
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
